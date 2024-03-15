db42d98d310c7a34856b3ec7c8968f9790da73fb
aastikn <aastiksimple@gmail.com>
2024-03-15 15:58:15 +0530

    feat:created product model and added one route

--- API/models/product.go
+++ API/models/product.go
@@ -1,1 +1,13 @@
 package models
+
+import "gorm.io/gorm"
+
+type Product struct {
+	gorm.Model
+	Name            string `gorm:"notnull;" json:"name"`
+	ProductCategory string `gorm:"notnull;" json:"product_category"`
+	//Price           float64 `gorm:"notnull" json:"price"`
+	PositiveRatings uint64 `gorm:"notnull;default:0;" json:"rating"`
+	NegativeRatings uint64 `gorm:"notnull;default:0;" json:"rating"`
+	NeutralRatings  uint64 `gorm:"notnull;default:0;" json:"rating"`
+}
--- API/routes/userRoutes/deleteUser.go
+++ API/routes/userRoutes/deleteUser.go
@@ -1,4 +1,4 @@
-package routes
+package userRoutes
 
 import (
 	"charcha-api/database"
